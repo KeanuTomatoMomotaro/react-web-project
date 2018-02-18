72dde8e68ccd3b5afa6f012dd1741db61446055a
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2018-02-18 10:34:06 +0700

    flux actions defined

---
+++ src/actions/GuestBookActions.js
@@ -0,0 +1,18 @@
+import dispatcher from '../Dispatcher/dispatcher';
+
+export function createGuestComment( cId, cAuthor, cTitle, cContent){
+    dispatcher.dispatch({
+        type: "CREATE_GUEST_COMMENT",
+        id: cId,
+        author: cAuthor,
+        title: cTitle,
+        content: cContent
+    })
+}
+
+export function deleteGuestComment( cId ){
+    dispatcher.dispatch({
+        type: "DELETE_GUEST_COMMENT",
+        id: cId,
+    })
+}
